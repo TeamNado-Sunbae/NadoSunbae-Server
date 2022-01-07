0fabc4249a7e049d69ae8ba37f39d5b003f1f89e
seol <suljiwon@naver.com>
2022-01-08 07:26:27 +0900

    [FEAT] update functions index

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,18 @@
-const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const serviceAccount = require("./nadosunbae-server-firebase-adminsdk-lzgu9-7c653db78a");
+const dotenv = require("dotenv");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+dotenv.config();
+
+let firebase;
+if (admin.apps.length === 0) {
+  firebase = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} else {
+  firebase = admin.app();
+}
+
+module.exports = {
+  api: require("./api"),
+};
