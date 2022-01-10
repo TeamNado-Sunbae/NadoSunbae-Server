1ad81c3019d8eecc37e8999e4c65db3496e6f4ac
seol <suljiwon@naver.com>
2022-01-10 18:05:19 +0900

    [FEAT] add index to db for initial setting

---
+++ functions/db/index.js
@@ -0,0 +1,14 @@
+// db와 상호작용하는 코드
+module.exports = {
+  //   userDB: require("./user"),
+  //   universityDB: require("./university"),
+  //   majorDB: require("./major"),
+  //   reviewPostDB: require("./reviewPost"),
+  //   classroomPostDB: require("./classroomPost"),
+  //   postTypeDB: require("./postType"),
+  //   tagDB: require("./tag"),
+  //   relationReviewPostTagDB: require("./relationReviewPostTag"),
+  //   likeDB: require("./like"),
+  //   imageDB: require("./image"),
+  //   commentDB: require("./comment"),
+};
