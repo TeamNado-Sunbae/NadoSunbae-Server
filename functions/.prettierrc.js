a646dfdd8ca46ac1eb9a653adeb700fe15775345
seol <suljiwon@naver.com>
2022-01-10 20:14:41 +0900

    [DOCS] update prettierrc

---
+++ functions/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: false,
+  trailingComma: "all",
+  arrowParens: "always",
+  printWidth: 80,
+  tabWidth: 2,
+};
