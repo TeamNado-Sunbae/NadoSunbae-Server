96495048c326af40b398b1f70163ea00e485794c
seol <suljiwon@naver.com>
2022-01-12 04:48:54 +0900

    [FEAT] add auth to routes index

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// router.use('/auth', require('./auth'));
+router.use("/auth", require("./auth"));
 
 module.exports = router;
