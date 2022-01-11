dafca4f5dcfe460a3f2fc4147d4a28b78bc42dcb
변주현 <juhyeon@byunjuhyeon-ui-MacBookPro.local>
2022-01-12 05:46:52 +0900

    [FEAT] add major route to index

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 // router.use('/auth', require('./auth'));
+router.use("/major", require("./major"));
 
 module.exports = router;
