5167adc999501135e3ade8ffa2acdbe629378732
juhyeon <wngus4296@swu.ac.kr>
2022-01-12 19:39:33 +0900

    [RENAME] change file name jwtHandler to jwtHandlers

--- functions/lib/jwtHandlers.js
+++ functions/lib/jwtHandlers.js
@@ -10,13 +10,13 @@ const options = {
   issuer: "nadoSunbae",
 };
 
-// id, email, name, idFirebase가 담긴 JWT를 발급합니다.
+// id, email, name, firebaseFire가 담긴 JWT를 발급합니다.
 const sign = (user) => {
   const payload = {
     id: user.id,
     email: user.email,
     name: user.name || null,
-    idFirebase: user.idFirebase,
+    firebaseId: user.firebaseId,
   };
 
   const result = {
