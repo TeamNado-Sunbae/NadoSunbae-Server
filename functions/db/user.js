1ffd2703d33c38e37e2ba156b5c246edbaeb9a88
eunji <eunjikim8784@gmail.com>
2022-01-13 03:45:53 +0900

    [FIX] fix addUser to createUser

--- functions/db/user.js
+++ functions/db/user.js
@@ -1,27 +1,40 @@
 const _ = require("lodash");
 const convertSnakeToCamel = require("../lib/convertSnakeToCamel");
 
-const addUser = async (client, email, nickname, universityId, firstMajorId, firstMajorStart, secondMajorId, secondMajorStart, firebaseId) => {
-    const { rows } = await client.query(
-      `
+const createUser = async (
+  client,
+  email,
+  nickname,
+  universityId,
+  firstMajorId,
+  firstMajorStart,
+  secondMajorId,
+  secondMajorStart,
+  firebaseId,
+) => {
+  const { rows } = await client.query(
+    `
       INSERT INTO "user"
       (email, nickname, university_id, first_major_id, first_major_start, second_major_id, second_major_start, firebase_id)
       VALUES
       ($1, $2, $3, $4, $5, $6, $7, $8)
       RETURNING *
       `,
-  
-      [email, nickname, universityId, firstMajorId, firstMajorStart, secondMajorId, secondMajorStart,firebaseId]
-    );
-    return convertSnakeToCamel.keysToCamel(rows[0]);
-  };
 
-  module.exports = {
-    // getAllUsers,
-    // getUserById,
-    // getUserByIdFirebase,
-    // getUserByEmail,
-    // updateUser,
-    // deleteUser,
-    addUser,
-  };
+    [
+      email,
+      nickname,
+      universityId,
+      firstMajorId,
+      firstMajorStart,
+      secondMajorId,
+      secondMajorStart,
+      firebaseId,
+    ],
+  );
+  return convertSnakeToCamel.keysToCamel(rows[0]);
+};
+
+module.exports = {
+  createUser,
+};
