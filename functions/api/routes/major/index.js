421e117d7461b71cd5729eb8199122a39eb5508d
juhyeon <wngus4296@swu.ac.kr>
2022-01-13 00:35:47 +0900

    [FEAT] add checkUser middleware

--- functions/api/routes/major/index.js
+++ functions/api/routes/major/index.js
@@ -3,6 +3,6 @@ const { checkUser } = require("../../../middlewares/auth");
 const router = express.Router();
 
 router.get("/list/:universityId", require("./majorListGET"));
-router.get("/:majorId", require("./majorGET"));
+router.get("/:majorId", checkUser, require("./majorGET"));
 
 module.exports = router;
