b26a3c9222fb5ed8afdbdc19796c027fdb92a9ec
seol <suljiwon@naver.com>
2022-01-14 05:57:06 +0900

    [FEAT] implement userListMajorGET

--- functions/db/user.js
+++ functions/db/user.js
@@ -108,6 +108,18 @@ const getUserByFirebaseId = async (client, firebaseId) => {
   return convertSnakeToCamel.keysToCamel(rows[0]);
 };
 
+const getUsersByMajorId = async (client, majorId) => {
+  const { rows } = await client.query(
+    `
+    SELECT * FROM "user" u
+    WHERE (u.first_major_id = $1 OR u.second_major_id = $1)
+    AND is_deleted = false
+        `,
+    [majorId],
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
+};
+
 module.exports = {
   getUserByNickname,
   createUser,
@@ -115,4 +127,5 @@ module.exports = {
   getUserByFirstMajorId,
   getUserByUserId,
   getUserByFirebaseId,
+  getUsersByMajorId,
 };
