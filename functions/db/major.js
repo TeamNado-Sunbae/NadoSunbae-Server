a7f245dec90abf5a9a4e9a86a9afecc2aea89463
juhyeon <wngus4296@swu.ac.kr>
2022-01-13 20:23:33 +0900

    [FIX] add deleted check, change fuction name

--- functions/db/major.js
+++ functions/db/major.js
@@ -1,15 +1,16 @@
 const _ = require("lodash");
 const convertSnakeToCamel = require("../lib/convertSnakeToCamel");
 
-const getDataByMajorId = async (client, majorId) => {
+const getMajorByMajorId = async (client, majorId) => {
   const { rows } = await client.query(
     `
     SELECT major_name, homepage, subject_table FROM "major" m
-    WHERE m.id = $1
+    WHERE m.id = $1,
+    AND is_deleted = false
     `,
     [majorId],
   );
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
-module.exports = { getDataByMajorId };
+module.exports = { getMajorByMajorId };
