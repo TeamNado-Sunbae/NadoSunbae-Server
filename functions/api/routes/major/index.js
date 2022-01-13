2433535948c02134b09d5edb46977a9261142707
juhyeon <wngus4296@swu.ac.kr>
2022-01-13 16:09:34 +0900

    [FIX] remove #7 functions to defend conflicts

--- functions/api/routes/major/index.js
+++ functions/api/routes/major/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { checkUser } = require("../../../middlewares/auth");
 const router = express.Router();
 
-router.get("/list/:universityId", require("./majorListGET"));
 router.get("/:majorId", checkUser, require("./majorGET"));
 
 module.exports = router;
