568933682155e3cdf0d4bd9263dfc18bdf03af55
juhyeon <wngus4296@swu.ac.kr>
2022-01-14 02:37:17 +0900

    [FIX] remove duplicated code

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -4,6 +4,5 @@ const router = express.Router();
 router.use("/auth", require("./auth"));
 router.use("/review-post", require("./reviewPost"));
 router.use("/major", require("./major"));
-router.use("/review-post", require("./reviewPost"));
 
 module.exports = router;
