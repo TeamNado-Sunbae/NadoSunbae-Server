5614d1a4de31032cfd284741fbe965c94789d887
juhyeon <wngus4296@swu.ac.kr>
2022-01-14 02:11:23 +0900

    [FEAT] add checkUser middleware

--- functions/api/routes/reviewPost/index.js
+++ functions/api/routes/reviewPost/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { checkUser } = require("../../../middlewares/auth");
 const router = express.Router();
 
-router.get("/background-image/list", require("./reviewPostBackgroundImageListGET"));
 router.post("/", checkUser, require("./reviewPostPOST"));
+router.get("/background-image/list", checkUser, require("./reviewPostBackgroundImageListGET"));
 
 module.exports = router;
