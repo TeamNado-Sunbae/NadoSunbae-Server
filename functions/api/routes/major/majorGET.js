c3009194f3ab3059827567e451e99e4e2825a64f
juhyeon <wngus4296@swu.ac.kr>
2022-01-13 16:04:27 +0900

    [FIX] change majorId of params check

--- functions/api/routes/major/majorGET.js
+++ functions/api/routes/major/majorGET.js
@@ -8,7 +8,7 @@ const { majorDB } = require("../../../db");
 module.exports = async (req, res) => {
   const { majorId } = req.params;
 
-  if (!majorId && majorId !== 0) {
+  if (!majorId) {
     return res
       .status(statusCode.BAD_REQUEST)
       .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
