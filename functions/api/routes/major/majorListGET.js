9d17d9d7c958160f479f88696a5dbfb291aacb51
juhyeon <wngus4296@swu.ac.kr>
2022-01-13 20:42:19 +0900

    [FIX] change status code when no data in majorList

--- functions/api/routes/major/majorListGET.js
+++ functions/api/routes/major/majorListGET.js
@@ -32,7 +32,7 @@ module.exports = async (req, res) => {
     if (!majorList) {
       return res
         .status(statusCode.BAD_REQUEST)
-        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+        .send(util.fail(statusCode.NO_CONTENT, responseMessage.NULL_VALUE));
     }
 
     res
