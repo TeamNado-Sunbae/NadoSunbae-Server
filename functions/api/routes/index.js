b3264e43d7c8eccac4f5bf3a7af245a435c9afad
seol <suljiwon@naver.com>
2022-01-14 18:21:47 +0900

    [REFACTOR] mypage routes to index

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/auth", require("./auth"));
-router.use("/user", require("./user"));
+router.use("/user/mypage", require("./user"));
 router.use("/review-post", require("./reviewPost"));
 router.use("/major", require("./major"));
 
--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -2,10 +2,6 @@ const express = require("express");
 const { checkUser } = require("../../../middlewares/auth");
 const router = express.Router();
 
-router.get(
-  "/mypage/:userId/classroom-post/list",
-  checkUser,
-  require("./userMypageClassroomPostListGET"),
-);
+router.get("/:userId/classroom-post/list", checkUser, require("./userMypageClassroomPostListGET"));
 
 module.exports = router;
