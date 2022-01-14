71b4f9b1291304bdb8a676ff85bf5549fb4083d5
eunji <eunjikim8784@gmail.com>
2022-01-14 15:26:20 +0900

    [FIX] add error handling

--- functions/api/routes/classroomPost/classroomPostPOST.js
+++ functions/api/routes/classroomPost/classroomPostPOST.js
@@ -9,10 +9,24 @@ module.exports = async (req, res) => {
   const { majorId, answererId, postTypeId, title, content } = req.body;
   let writer = req.user;
 
-  if (!majorId || !answererId || !postTypeId || !title || !content) {
+  if (!majorId || !postTypeId || !title || !content) {
     return res
       .status(statusCode.BAD_REQUEST)
       .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  }
+
+  if (postTypeId == 4) {
+    if (!answererId) {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    }
+  }
+
+  if (writer.isReviewed == false) {
+    return res
+      .status(statusCode.FORBIDDEN)
+      .send(util.fail(statusCode.FORBIDDEN, responseMessage.IS_REVIEWD_FALSE));
   }
 
   let client;
