1c8805054df0eb53303236291566366c2e97ac5f
juhyeon <wngus4296@swu.ac.kr>
2022-01-16 00:57:56 +0900

    [DELETE] delete code if 403 error

--- functions/api/routes/comment/commentDELETE.js
+++ functions/api/routes/comment/commentDELETE.js
@@ -19,14 +19,6 @@ module.exports = async (req, res) => {
   try {
     client = await db.connect(req);
 
-    const comment = await commentDB.getCommentByCommentId(client, commentId);
-
-    if (comment.writerId !== req.user.id) {
-      return res
-        .status(statusCode.FORBIDDEN)
-        .send(util.fail(statusCode.NOT_FOUND, responseMessage.FORBIDDEN_ACCESS));
-    }
-
     let deletedComment = await commentDB.deleteCommentByCommentId(client, commentId);
 
     res
