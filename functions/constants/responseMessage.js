928ddd165cd9f4c7baf1204f523b9498189b17db
juhyeon <wngus4296@swu.ac.kr>
2022-01-17 02:09:37 +0900

    [FIX] remove duplicate response message

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -56,7 +56,6 @@ module.exports = {
   NO_AUTH_HEADER: "Authorization 헤더가 없습니다.",
   FORBIDDEN_ACCESS: "사용 권한이 없습니다.",
   IS_REVIEWED_FALSE: "후기글 미등록자입니다.",
-  FORBIDDEN_ACCESS: "사용 권한이 없습니다.",
 
   // 전공 목록 조회
   READ_ALL_MAJORS_SUCCESS: "해당 학교의 학과 목록 조회 성공",
@@ -76,7 +75,7 @@ module.exports = {
 
   // 좋아요
   UPDATE_LIKE_SUCCESS: "포스트 좋아요 업데이트 성공",
-  
+
   // 신고 성공
   REPORT_SUCCESS: "신고 성공",
 };
