137c67db8c185d2d30c83b29358bcc0b563c71fc
seol <suljiwon@naver.com>
2022-01-17 23:05:05 +0900

    [FIX] prevent no content

--- functions/api/routes/comment/commentPUT.js
+++ functions/api/routes/comment/commentPUT.js
@@ -10,7 +10,7 @@ module.exports = async (req, res) => {
   const { commentId } = req.params;
   const { content } = req.body;
 
-  if (!commentId)
+  if (!commentId || !content)
     return res
       .status(statusCode.BAD_REQUEST)
       .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
