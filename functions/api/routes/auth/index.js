23347144fb96c445c58551e0ce4f7e211d0cb689
juhyeon <wngus4296@swu.ac.kr>
2022-01-19 04:59:18 +0900

    [FEAT] add auto login by access token

--- functions/api/routes/auth/index.js
+++ functions/api/routes/auth/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const { checkUser } = require("../../../middlewares/auth");
 const router = express.Router();
 
 router.post("/duplication-check/nickname", require("./authDuplicationCheckNicknamePOST"));
 router.post("/duplication-check/email", require("./authDuplicationCheckEmailPOST"));
 router.post("/signup", require("./authSignupPOST"));
 router.post("/login", require("./authLoginPOST"));
+router.post("/login/token", checkUser, require("./authLoginTokenPOST"));
 
 module.exports = router;
