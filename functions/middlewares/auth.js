386a9a75c3e94caf2163534025b473b44eef0ecf
juhyeon <wngus4296@swu.ac.kr>
2022-02-09 03:14:12 +0900

    [FEAT] accesstoken, refreshtoken req 분기처리

--- functions/middlewares/auth.js
+++ functions/middlewares/auth.js
@@ -20,80 +20,122 @@ const checkUser = async (req, res, next) => {
   try {
     client = await db.connect(req);
 
-    // token 해독
-    let decodedAccessToken = jwtHandlers.verify(accesstoken);
-    let decodedRefreshToken = jwtHandlers.verify(refreshtoken);
+    // accesstoken만 보냈을 때
+    if (!refreshtoken) {
+      // jwt를 해독하고 인증 절차를 거칩니다.
+      const decodedToken = jwtHandlers.verify(accesstoken);
 
-    // 토큰 만료 확인 및 재발급
-    if (decodedAccessToken === TOKEN_EXPIRED) {
-      // access, refresh token 둘 다 만료 (재로그인 필요)
-      if (decodedRefreshToken === TOKEN_EXPIRED) {
+      // jwt가 만료되었거나 잘못되었을 시의 에러 처리입니다.
+      if (decodedToken === TOKEN_EXPIRED)
         return res
           .status(statusCode.UNAUTHORIZED)
           .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_EXPIRED));
-      }
-      // access token만 만료
-      const userData = await userDB.getUserByRefreshToken(client, refreshtoken);
-      if (userData.id) {
-        // acesstoken 재발급
-        const { accesstoken } = jwtHandlers.sign(userData);
-        return res.status(statusCode.OK).send(
-          util.success(statusCode.OK, responseMessage.UPDATE_TOKEN_SUCCESS, {
-            newAccesstoken: accesstoken,
-          }),
-        );
-      }
-    } else if (decodedAccessToken === TOKEN_INVALID) {
-      return res
-        .status(statusCode.UNAUTHORIZED)
-        .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_INVALID));
-    }
-    // accesstoken 유효한 경우
-    else {
-      // refreshtoken 만료된 경우
-      if (decodedRefreshToken === TOKEN_EXPIRED) {
-        // refreshtoken 재발급 및 db에 저장
-        const { refreshtoken } = jwtHandlers.refresh();
-        const updatedUserByRefreshToken = await userDB.updateUserByRefreshToken(
-          client,
-          decodedAccessToken.id,
-          refreshtoken,
-        );
-        if (!updatedUserByRefreshToken) {
+      if (decodedToken === TOKEN_INVALID)
+        return res
+          .status(statusCode.UNAUTHORIZED)
+          .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_INVALID));
+
+      // 해독된 jwt에 담긴 id 값이 우리가 DB에서 찾고자 하는 user의 id입니다.
+      const userId = decodedToken.id;
+      // 유저id가 없을 시의 에러 처리입니다.
+      if (!userId)
+        return res
+          .status(statusCode.UNAUTHORIZED)
+          .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_INVALID));
+
+      // 위의 id 값으로 유저를 조회합니다.
+      const user = await userDB.getUserByUserId(client, userId);
+
+      // 유저가 없을 시의 에러 처리입니다.
+      if (!user)
+        return res
+          .status(statusCode.UNAUTHORIZED)
+          .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.NO_USER));
+
+      // 유저를 찾았으면, req.user에 유저 객체를 담아서 next()를 이용해 다음 middleware로 보냅니다.
+      // 다음 middleware는 req.user에 담긴 유저 정보를 활용할 수 있습니다.
+      req.user = user;
+      next();
+    } else {
+      // token 해독
+      let decodedAccessToken = jwtHandlers.verify(accesstoken);
+      let decodedRefreshToken = jwtHandlers.verify(refreshtoken);
+
+      // 토큰 만료 확인 및 재발급
+      if (decodedAccessToken === TOKEN_EXPIRED) {
+        // access, refresh token 둘 다 만료 (재로그인 필요)
+        if (decodedRefreshToken === TOKEN_EXPIRED) {
           return res
-            .status(statusCode.INTERNAL_SERVER_ERROR)
-            .send(
-              util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.UPDATE_DEVICE_TOKEN_FAIL),
-            );
+            .status(statusCode.UNAUTHORIZED)
+            .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_EXPIRED));
         }
-        return res.status(statusCode.OK).send(
-          util.success(statusCode.OK, responseMessage.UPDATE_TOKEN_SUCCESS, {
-            newRefreshtoken: refreshtoken,
-          }),
-        );
+        // access token만 만료
+        const userData = await userDB.getUserByRefreshToken(client, refreshtoken);
+        if (userData.id) {
+          // acesstoken 재발급
+          const { accesstoken } = jwtHandlers.sign(userData);
+          return res.status(statusCode.OK).send(
+            util.success(statusCode.OK, responseMessage.UPDATE_TOKEN_SUCCESS, {
+              newAccesstoken: accesstoken,
+            }),
+          );
+        }
+      } else if (decodedAccessToken === TOKEN_INVALID) {
+        return res
+          .status(statusCode.UNAUTHORIZED)
+          .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_INVALID));
       }
+      // accesstoken 유효한 경우
+      else {
+        // refreshtoken 만료된 경우
+        if (decodedRefreshToken === TOKEN_EXPIRED) {
+          // refreshtoken 재발급 및 db에 저장
+          const { refreshtoken } = jwtHandlers.refresh();
+          const updatedUserByRefreshToken = await userDB.updateUserByRefreshToken(
+            client,
+            decodedAccessToken.id,
+            refreshtoken,
+          );
+          if (!updatedUserByRefreshToken) {
+            return res
+              .status(statusCode.INTERNAL_SERVER_ERROR)
+              .send(
+                util.fail(
+                  statusCode.INTERNAL_SERVER_ERROR,
+                  responseMessage.UPDATE_DEVICE_TOKEN_FAIL,
+                ),
+              );
+          }
+          return res.status(statusCode.OK).send(
+            util.success(statusCode.OK, responseMessage.UPDATE_TOKEN_SUCCESS, {
+              newRefreshtoken: refreshtoken,
+            }),
+          );
+        }
+      }
+
+      // 해독된 jwt에 담긴 id 값이 우리가 DB에서 찾고자 하는 user의 id입니다.
+      const userId = decodedAccessToken.id;
+      // 유저id가 없을 시의 에러 처리입니다.
+      if (!userId)
+        return res
+          .status(statusCode.UNAUTHORIZED)
+          .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_INVALID));
+
+      // 위의 id 값으로 유저를 조회합니다.
+      const user = await userDB.getUserByUserId(client, userId);
+
+      // 유저가 없을 시의 에러 처리입니다.
+      if (!user)
+        return res
+          .status(statusCode.UNAUTHORIZED)
+          .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.NO_USER));
+
+      // 유저를 찾았으면, req.user에 유저 객체를 담아서 next()를 이용해 다음 middleware로 보냅니다.
+      // 다음 middleware는 req.user에 담긴 유저 정보를 활용할 수 있습니다.
+      req.user = user;
+      next();
     }
-    // 해독된 jwt에 담긴 id 값이 우리가 DB에서 찾고자 하는 user의 id입니다.
-    const userId = decodedAccessToken.id;
-    // 유저id가 없을 시의 에러 처리입니다.
-    if (!userId)
-      return res
-        .status(statusCode.UNAUTHORIZED)
-        .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_INVALID));
-
-    // 위의 id 값으로 유저를 조회합니다.
-    const user = await userDB.getUserByUserId(client, userId);
-
-    // 유저가 없을 시의 에러 처리입니다.
-    if (!user)
-      return res
-        .status(statusCode.UNAUTHORIZED)
-        .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.NO_USER));
-
-    // 유저를 찾았으면, req.user에 유저 객체를 담아서 next()를 이용해 다음 middleware로 보냅니다.
-    // 다음 middleware는 req.user에 담긴 유저 정보를 활용할 수 있습니다.
-    req.user = user;
-    next();
   } catch (error) {
     console.log(error);
     functions.logger.error(
