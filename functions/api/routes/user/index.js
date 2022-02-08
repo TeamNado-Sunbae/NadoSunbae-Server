fe7a3eb6352c342fc94b39a9a3ef778aa5f76bbe
seol <suljiwon@naver.com>
2022-02-08 20:56:57 +0900

    [REFACTOR] refactor mypage api

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const { checkUser } = require("../../../middlewares/auth");
 const router = express.Router();
 
-router.get("/", checkUser, require("./userMypageGET"));
-router.get("/:userId", checkUser, require("./userMypageOtherGET"));
+router.get("/:userId", checkUser, require("./userMypageGET"));
 router.get("/:userId/classroom-post/list", checkUser, require("./userMypageClassroomPostListGET"));
 router.get("/list/major/:majorId", checkUser, require("./userListMajorGET"));
 
