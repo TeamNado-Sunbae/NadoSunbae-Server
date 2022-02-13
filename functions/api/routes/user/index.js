20c5a5a904cb73705aedf5e0f1dac2908ea6131e
juhyeon <wngus4296@swu.ac.kr>
2022-02-14 00:15:41 +0900

    [FIX] match file name and router

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.get("/:userId", checkUser, require("./userMypageGET"));
 router.get("/:userId/classroom-post/list", checkUser, require("./userMypageClassroomPostListGET"));
 router.get("/list/major/:majorId", checkUser, require("./userListMajorGET"));
-router.get("/app-version/recent", checkUser, require("./userMypageAppVersionGET"));
+router.get("/app-version/recent", checkUser, require("./userMypageAppVersionRecentGET"));
 
 module.exports = router;
