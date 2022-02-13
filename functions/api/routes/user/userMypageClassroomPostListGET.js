44d461f9a0cc4f9b48d73e59517e89f851c2b029
juhyeon <wngus4296@swu.ac.kr>
2022-02-14 03:39:24 +0900

    [FEAT] filter error handling

--- functions/api/routes/user/userMypageClassroomPostListGET.js
+++ functions/api/routes/user/userMypageClassroomPostListGET.js
@@ -38,6 +38,10 @@ module.exports = async (req, res) => {
         req.user.id,
         [3, 4],
       );
+    } else {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.INCORRECT_FILTER));
     }
 
     classroomPostList = await Promise.all(
