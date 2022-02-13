7724e547a8bdcc0e535d6d99e3bf1f100c3db0f9
eunji <eunjikim8784@gmail.com>
2022-02-14 00:55:47 +0900

    [FEAT] add get router to index

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get("/:userId", checkUser, require("./userMypageGET"));
 router.get("/:userId/classroom-post/list", checkUser, require("./userMypageClassroomPostListGET"));
 router.get("/list/major/:majorId", checkUser, require("./userListMajorGET"));
+router.get("/comment/list/:postTypeId", checkUser, require("./userMypageCommentListGET"));
 
 module.exports = router;
