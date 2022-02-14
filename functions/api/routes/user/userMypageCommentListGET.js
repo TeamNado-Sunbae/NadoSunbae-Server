cfc21810f79b6f4b6496c85f591590b9a0567022
eunji <eunjikim8784@gmail.com>
2022-02-15 00:45:03 +0900

    [DELETE] delete useless code

--- functions/api/routes/user/userMypageCommentListGET.js
+++ functions/api/routes/user/userMypageCommentListGET.js
@@ -13,7 +13,6 @@ module.exports = async (req, res) => {
       .status(statusCode.BAD_REQUEST)
       .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
   }
-  postTypeId = Number(postTypeId);
 
   let client;
 
