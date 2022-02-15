6a4ae3ad55008cd7ae201fe33dcf3f8be6e2cdf8
juhyeon <wngus4296@swu.ac.kr>
2022-02-15 13:57:00 +0900

    [REFACTOR] speed improvement

--- functions/api/routes/auth/authSignupPOST.js
+++ functions/api/routes/auth/authSignupPOST.js
@@ -96,20 +96,6 @@ module.exports = async (req, res) => {
       createdAt: user.createdAt,
     };
 
-    // 로그인 및 메일 전송
-    await signInWithEmailAndPassword(firebaseAuth, email, password)
-      .then(() => sendEmailVerification(firebaseAuth.currentUser))
-      .catch((e) => {
-        return res
-          .status(statusCode.INTERNAL_SERVER_ERROR)
-          .send(
-            util.fail(
-              statusCode.INTERNAL_SERVER_ERROR,
-              responseMessage.SEND_VERIFICATION_EMAIL_FAIL,
-            ),
-          );
-      });
-
     res.status(statusCode.OK).send(
       util.success(
         statusCode.OK,
@@ -118,6 +104,11 @@ module.exports = async (req, res) => {
           user,
         },
       ),
+    );
+
+    // 로그인 및 메일 전송
+    await signInWithEmailAndPassword(firebaseAuth, email, password).then(() =>
+      sendEmailVerification(firebaseAuth.currentUser),
     );
   } catch (error) {
     console.log(error);
