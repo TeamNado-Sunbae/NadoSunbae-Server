e22a88ba05badbf3268ef04df6ea8d79fb830e58
seol <suljiwon@naver.com>
2022-02-15 11:15:21 +0900

    [FEAT] implement reset password

--- functions/api/routes/auth/index.js
+++ functions/api/routes/auth/index.js
@@ -7,6 +7,7 @@ router.post("/duplication-check/email", require("./authDuplicationCheckEmailPOST
 router.post("/signup", require("./authSignupPOST"));
 router.post("/login", require("./authLoginPOST"));
 router.post("/renewal/token", require("./authRenewalTokenPOST"));
+router.post("/reset/password", require("./authResetPasswordPOST"));
 router.get("/university/:universityId", require("./authUniversityGET"));
 router.post("/certification/email", require("./authCertificationEmailPOST"));
 
