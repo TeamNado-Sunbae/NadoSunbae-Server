2272df4b09808ca3bd06bca83e2ac7dbb424713f
eunji <eunjikim8784@gmail.com>
2022-02-17 23:21:11 +0900

    [REFACTOR] improve code quality

--- functions/api/routes/user/userMypagePUT.js
+++ functions/api/routes/user/userMypagePUT.js
@@ -28,17 +28,18 @@ module.exports = async (req, res) => {
   try {
     client = await db.connect(req);
 
-    // 닉네임 중복 확인
-    const existingUser = await userDB.getUserByNickname(client, nickname);
-    // 유저가 닉네임 변경을 안 했을 경우에는 중복 검사 안 함
-    if (existingUser && existingUser.id !== req.user.id) {
-      return res
-        .status(statusCode.CONFLICT)
-        .send(util.fail(statusCode.CONFLICT, responseMessage.ALREADY_NICKNAME));
-    }
-
     // 닉네임 변경했을 때만 nickname_updated_at 업데이트
     const isNicknameUpdated = req.user.nickname !== nickname ? true : false;
+
+    if (isNicknameUpdated) {
+      // 닉네임 중복 확인
+      const existingUser = await userDB.getUserByNickname(client, nickname);
+      if (existingUser) {
+        return res
+          .status(statusCode.CONFLICT)
+          .send(util.fail(statusCode.CONFLICT, responseMessage.ALREADY_NICKNAME));
+      }
+    }
 
     // 유저 정보 수정
     let updatedUser = await userDB.updateUserByMypage(
