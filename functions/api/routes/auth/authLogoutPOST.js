27bdcffbc04bd1f67d44163df851dada6b708647
eunji <eunjikim8784@gmail.com>
2022-02-18 21:43:52 +0900

    [FEAT] add logout post

---
+++ functions/api/routes/auth/authLogoutPOST.js
@@ -0,0 +1,49 @@
+const functions = require("firebase-functions");
+const db = require("../../../db/db");
+const util = require("../../../lib/util");
+const statusCode = require("../../../constants/statusCode");
+const responseMessage = require("../../../constants/responseMessage");
+const { userDB } = require("../../../db");
+const slackAPI = require("../../../middlewares/slackAPI");
+
+module.exports = async (req, res) => {
+  let client;
+  try {
+    client = await db.connect(req);
+
+    // 리프레쉬토큰 업데이트
+    let refreshTokenUpdated = await userDB.updateUserByLogout(client, req.user.id);
+    if (!refreshTokenUpdated) {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.UPDATE_REFRESH_TOKEN_FAIL));
+    }
+
+    refreshTokenUpdated = {
+      userId: refreshTokenUpdated.id,
+      refreshToken: refreshTokenUpdated.refreshToken,
+      updatedAt: refreshTokenUpdated.updatedAt,
+    };
+
+    res
+      .status(statusCode.OK)
+      .send(util.success(statusCode.OK, responseMessage.LOGOUT_SUCCESS, { refreshTokenUpdated }));
+  } catch (error) {
+    functions.logger.error(
+      `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+      `[CONTENT] ${error}`,
+    );
+    console.log(error);
+
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${
+      req.originalUrl
+    } ${error} ${JSON.stringify(error)}`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};
