a8de87c026444d607263a370927b71a15fa9451e
seol <suljiwon@naver.com>
2022-02-19 22:51:26 +0900

    [FEAT] add getExpirationDateByMonth

--- functions/lib/dateHandlers.js
+++ functions/lib/dateHandlers.js
@@ -7,14 +7,14 @@ const getCurrentUTCNumberDate = () => {
 };
 
 // 현재 날짜를 미국 표준시로 반환
-// return type: object (Date)
+// return type: object(Date)
 const getCurrentUTCDate = () => {
   const UTCNow = new Date(getCurrentUTCNumberDate());
   return UTCNow;
 };
 
 // 현재 날짜를 한국 표준시로 반환
-// return type: object (Date)
+// return type: object(Date)
 const getCurrentKSTDate = () => {
   const UTCNow = getCurrentUTCNumberDate();
 
@@ -23,8 +23,18 @@ const getCurrentKSTDate = () => {
   return KSTNow;
 };
 
+// month만큼 지난 후 만료되는 날짜를 반환
+// input type: baseDate - object(Date), expirationMonth(number)
+// return type: object(Date)
+const getExpirationDateByMonth = (baseDate, expirationMonth) => {
+  // setMonth parameter는 1 월에서 12 월까지의 월을 나타내는 0에서 11 사이의 정수
+  const expirationDate = new Date(baseDate.setMonth(baseDate.getMonth() + expirationMonth));
+  return expirationDate;
+};
+
 module.exports = {
   getCurrentUTCNumberDate,
   getCurrentUTCDate,
   getCurrentKSTDate,
+  getExpirationDateByMonth,
 };
