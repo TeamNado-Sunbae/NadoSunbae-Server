9bdc828c50694c9952f35a9a0d295b9d316a730e
juhyeon <wngus4296@swu.ac.kr>
2022-02-20 18:28:19 +0900

    [FIX] refactor error handling

--- functions/api/routes/auth/authCertificationEmailPOST.js
+++ functions/api/routes/auth/authCertificationEmailPOST.js
@@ -21,18 +21,19 @@ module.exports = async (req, res) => {
     client = await db.connect(req);
 
     // 로그인 및 메일 전송
-    await signInWithEmailAndPassword(firebaseAuth, email, password)
+    const sentEmail = await signInWithEmailAndPassword(firebaseAuth, email, password)
       .then(() => sendEmailVerification(firebaseAuth.currentUser))
-      .catch((e) => {
-        return res
-          .status(statusCode.INTERNAL_SERVER_ERROR)
-          .send(
-            util.fail(
-              statusCode.INTERNAL_SERVER_ERROR,
-              responseMessage.SEND_VERIFICATION_EMAIL_FAIL,
-            ),
-          );
+      .catch(() => {
+        return { err: true };
       });
+
+    if (sentEmail.err) {
+      return res
+        .status(statusCode.INTERNAL_SERVER_ERROR)
+        .send(
+          util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.SEND_VERIFICATION_EMAIL_FAIL),
+        );
+    }
 
     res
       .status(statusCode.OK)
