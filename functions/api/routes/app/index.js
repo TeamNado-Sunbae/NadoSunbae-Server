1577b545266fa4c2878c08ef1aab5bbfdd2f11e6
seol <suljiwon@naver.com>
2022-03-03 01:31:03 +0900

    [FIX] delete check user

--- functions/api/routes/app/index.js
+++ functions/api/routes/app/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { checkUser } = require("../../../middlewares/auth");
 const router = express.Router();
 
-router.get("/link", checkUser, require("./appLinkGET"));
+router.get("/link", require("./appLinkGET"));
 
 module.exports = router;
