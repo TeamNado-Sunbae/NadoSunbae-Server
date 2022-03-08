648cfeea185338e8252290c61a7ff3a6c59e1de7
eunji <eunjikim8784@gmail.com>
2022-03-09 01:13:00 +0900

    [TEST] modify accesstoken expiresIn to 1m

--- functions/lib/jwtHandlers.js
+++ functions/lib/jwtHandlers.js
@@ -18,7 +18,7 @@ const access = (user) => {
     // accesstoken 발급
     accesstoken: jwt.sign(payload, secretKey, {
       algorithm: "HS256",
-      expiresIn: "3m",
+      expiresIn: "1m",
       issuer: "nadoSunbae",
     }),
   };
