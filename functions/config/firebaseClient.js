08f7aa52bf686c5f5da83c7fbe5024970590acd9
seol <suljiwon@naver.com>
2022-03-16 05:05:03 +0900

    [CHORE] add dev firebase config, dev service account

--- functions/config/firebaseClient.js
+++ functions/config/firebaseClient.js
@@ -1,7 +1,7 @@
 const { initializeApp } = require("firebase/app");
 const { getAuth } = require("firebase/auth");
 
-const firebaseConfig = {
+const prodFirebaseConfig = {
   apiKey: "AIzaSyBEqiqaiIR7czG5XLBgxgbFERioHa6kGJ0",
   authDomain: "nadosunbae-server.firebaseapp.com",
   projectId: "nadosunbae-server",
@@ -10,6 +10,19 @@ const firebaseConfig = {
   appId: "1:473721519726:web:9c1a172885f4c6e479307f",
   measurementId: "G-QG47JRQXY7",
 };
+
+const devFirebaseConfig = {
+  apiKey: "AIzaSyA1bFdLq0jfVfBhb3EihIgw5WJ-KIeNVwI",
+  authDomain: "nadosunbae-server-dev-90ac3.firebaseapp.com",
+  projectId: "nadosunbae-server-dev-90ac3",
+  storageBucket: "nadosunbae-server-dev-90ac3.appspot.com",
+  messagingSenderId: "978556585705",
+  appId: "1:978556585705:web:7539681d09460699db9278",
+  measurementId: "G-YL3225MTLE",
+};
+
+const firebaseConfig =
+  process.env.NODE_ENV === "production" ? prodFirebaseConfig : devFirebaseConfig;
 
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
