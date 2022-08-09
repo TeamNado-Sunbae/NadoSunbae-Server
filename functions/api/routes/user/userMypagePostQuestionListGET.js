1f71f3b7d7c64bbeb0130415722f5e96e4aac1d4
seoljiwon <suljiwon@naver.com>
2022-08-09 23:53:28 +0900

    [FEAT] implement my page question list

--- functions/api/routes/user/userMypagePostQuestionListGET.js
+++ functions/api/routes/user/userMypagePostQuestionListGET.js
@@ -3,7 +3,7 @@ const util = require("../../../lib/util");
 const statusCode = require("../../../constants/statusCode");
 const responseMessage = require("../../../constants/responseMessage");
 const db = require("../../../db/db");
-const { postDB, likeDB, userDB, commentDB, blockDB } = require("../../../db");
+const { postDB, blockDB } = require("../../../db");
 const { likeType } = require("../../../constants/type");
 const errorHandlers = require("../../../lib/errorHandlers");
 
@@ -26,48 +26,31 @@ module.exports = async (req, res) => {
     const invisibleUserList = await blockDB.getInvisibleUserListByUserId(client, req.user.id);
     const invisibleUserIds = _.map(invisibleUserList, "userId");
 
-    let postList = await postDB.getPostListByUserId(client, userId, invisibleUserIds);
-
-    // postList에 작성자 정보와 댓글 개수, 좋아요 개수를 붙임
-    postList = await Promise.all(
-      postList.map(async (post) => {
-        let writer = await userDB.getUserByUserId(client, post.writerId);
-        // 작성자 정보
-        writer = {
-          writerId: writer.id,
-          profileImageId: writer.profileImageId,
-          nickname: writer.nickname,
-        };
-
-        // 댓글 개수
-        const commentCount = await commentDB.getCommentCountByPostId(
-          client,
-          post.id,
-          invisibleUserIds,
-        );
-
-        // 좋아요 정보
-        const likeData = await likeDB.getLikeByTarget(client, post.id, likeType.POST, req.user.id);
-
-        const isLiked = likeData ? likeData.isLiked : false;
-
-        const likeCount = await likeDB.getLikeCountByTarget(client, post.id, likeType.POST);
-        const like = {
-          isLiked: isLiked,
-          likeCount: likeCount.likeCount,
-        };
-
-        return {
-          postId: post.id,
-          title: post.title,
-          content: post.content,
-          createdAt: post.createdAt,
-          writer: writer,
-          commentCount: commentCount.commentCount,
-          like: like,
-        };
-      }),
+    let postList = await postDB.getPostListByAnswererId(
+      client,
+      userId,
+      req.user.id,
+      likeType.POST,
+      invisibleUserIds,
     );
+
+    postList = postList.map((post) => {
+      return {
+        postId: post.id,
+        title: post.title,
+        content: post.content,
+        createdAt: post.createdAt,
+        writer: {
+          id: post.writerId,
+          nickname: post.nickname,
+        },
+        commentCount: post.commentCount,
+        like: {
+          isLiked: post.isLiked,
+          likeCount: post.likeCount,
+        },
+      };
+    });
 
     if (sort === "recent") {
       postList = _.sortBy(postList, "createdAt").reverse();
--- functions/db/post.js
+++ functions/db/post.js
@@ -14,16 +14,54 @@ const deletePostByPostId = async (client, postId) => {
   return convertSnakeToCamel.keysToCamel(rows[0]);
 };
 
-const getPostListByUserId = async (client, userId, invisibleUserIds) => {
+const getPostListByAnswererId = async (
+  client,
+  answererId,
+  userId,
+  likeTypeId,
+  invisibleUserIds,
+) => {
   const { rows } = await client.query(
     `
-      SELECT * FROM post
-      WHERE answerer_id = $1
-      AND writer_id <> all (ARRAY[${invisibleUserIds.join()}]::int[])
-      AND is_deleted = false
-      ORDER BY created_at desc
+      SELECT p.id, p.post_type_id, p.title, p.content, p.created_at, p.writer_id, u.nickname, m.major_name,
+      (
+        SELECT cast(count(c.*) as integer) comment_count FROM comment c
+        WHERE c.post_id = p.id
+        AND c.writer_id <> all (ARRAY[${invisibleUserIds.join()}]::int[])
+        AND c.is_deleted = false
+        AND p.is_deleted = false
+      ),
+      (
+        SELECT cast(count(l.*) as integer) AS like_count FROM "like" l
+        WHERE l.target_id = p.id
+        AND l.target_type_id = $3
+        AND l.is_liked = true
+        AND p.is_deleted = false
+      ),
+      (
+        coalesce(
+          (
+            SELECT l.is_liked FROM "like" l
+            WHERE l.target_id = p.id
+            AND l.target_type_id = $3
+            AND l.user_id = $2
+            AND p.is_deleted = false
+          ), false
+        )
+      ) as is_liked
+      FROM post p
+      INNER JOIN major m
+      ON p.major_id = m.id
+      AND m.is_deleted = false
+      INNER JOIN "user" u
+      ON p.writer_id = u.id
+      AND u.is_deleted = false
+      WHERE p.answerer_id = $1
+      AND p.writer_id <> all (ARRAY[${invisibleUserIds.join()}]::int[])
+      AND p.is_deleted = false
+      ORDER BY p.created_at desc
           `,
-    [userId],
+    [answererId, userId, likeTypeId],
   );
   return convertSnakeToCamel.keysToCamel(rows);
 };
@@ -184,7 +222,7 @@ const getPostListByLike = async (client, userId, likeTypeId, postTypeIds, invisi
 module.exports = {
   createPost,
   deletePostByPostId,
-  getPostListByUserId,
+  getPostListByAnswererId,
   getPostListByMajorId,
   getPostByPostId,
   updatePost,
