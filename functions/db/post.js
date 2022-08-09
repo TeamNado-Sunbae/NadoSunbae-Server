6bc99824cb9ec10e1775a83aea3206a7f3e2553b
seoljiwon <76513385+seoljiwon@users.noreply.github.com>
2022-08-10 01:43:32 +0900

    my page (#330)
    * refactor: refactor type constants
    
    * fix: change params, query, body type to string
    
    * fix: fix db column
    
    * feat: add error handler
    
    * fix: add req to error handler
    
    * [FIX] fix import
    
    * [FEAT] implement calculate response rate
    
    * [FEAT] implement my page

--- functions/db/post.js
+++ functions/db/post.js
@@ -327,6 +327,23 @@ const getPostListByComment = async (
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
+// response rate policy (answered questionToPerson post cnt/questionToPerson post cnt) * 100
+const calculateResponseRate = async (client, userId) => {
+  const { rows } = await client.query(
+    `
+    SELECT COUNT(DISTINCT CASE WHEN p.answerer_id = c.writer_id THEN p.id END)*100/COUNT(DISTINCT p.id) rate
+    FROM post p
+    LEFT JOIN "comment" c
+    ON c.post_id = p.id
+    AND c.is_deleted = false
+    where p.is_deleted = false
+    AND p.answerer_id = $1
+      `,
+    [userId],
+  );
+  return convertSnakeToCamel.keysToCamel(rows[0]);
+};
+
 module.exports = {
   createPost,
   deletePostByPostId,
@@ -338,5 +355,6 @@ module.exports = {
   deletePostListByUserSecession,
   getPostListByLike,
   getPostListByNotification,
+  calculateResponseRate,
   getPostListByComment,
 };
