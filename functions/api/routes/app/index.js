75b32b9bfb63a7c8813174ca5cf475b375fe7025
juhyeon <wngus4296@swu.ac.kr>
2022-08-13 23:25:56 +0900

    [REMOVE] remove unused code

--- functions/api/routes/app/index.js
+++ functions/api/routes/app/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { checkUser } = require("../../../middlewares/auth");
 
 router.get("/link", require("./appLinkGET"));
 router.get("/banner", require("./bannerListGET"));
