e367b6a78d8d4aae558b4d9b80d2764c4fb4c9a9
seoljiwon <76513385+seoljiwon@users.noreply.github.com>
2022-08-15 23:43:58 +0900

    sort major list (#340)
    * refactor: add major constants
    
    * [FEAT] filter excluded major
    
    * [FEAT] sort major
    
    * [COMMENT] delete new line
    
    * [FEAT] major constant id to name

--- functions/api/routes/major/majorListGET.js
+++ functions/api/routes/major/majorListGET.js
@@ -1,13 +1,15 @@
+const _ = require("lodash");
 const util = require("../../../lib/util");
 const statusCode = require("../../../constants/statusCode");
 const responseMessage = require("../../../constants/responseMessage");
 const db = require("../../../db/db");
 const { majorDB } = require("../../../db");
 const errorHandlers = require("../../../lib/errorHandlers");
+const { NO_INFO, NOT_ENTERED, NO_MAJOR } = require("../../../constants/major");
 
 module.exports = async (req, res) => {
   const { universityId } = req.params;
-  const { filter } = req.query;
+  const { filter, exclude } = req.query;
 
   if (!universityId || !filter) {
     return res
@@ -20,39 +22,69 @@ module.exports = async (req, res) => {
   try {
     client = await db.connect(req);
 
-    let majorList;
+    // filter & exclude query
+    let isFirstMajor, isSecondMajor, invisibleMajorNames;
 
     if (filter === "all") {
-      majorList = await majorDB.getMajorListByUniversityId(
-        client,
-        universityId,
-        [true, false],
-        [true, false],
-      );
-      majorList.shift();
+      isFirstMajor = [true, false];
+      isSecondMajor = [true, false];
+
+      if (exclude === "noMajor") {
+        invisibleMajorNames = [...NO_INFO, ...NOT_ENTERED, ...NO_MAJOR];
+      } else {
+        invisibleMajorNames = [...NO_INFO, ...NOT_ENTERED];
+      }
     } else if (filter === "firstMajor") {
-      majorList = await majorDB.getMajorListByUniversityId(
-        client,
-        universityId,
-        [true],
-        [true, false],
-      );
+      isFirstMajor = [true];
+      isSecondMajor = [true, false];
+      invisibleMajorNames = [...NO_INFO, ...NO_MAJOR];
     } else if (filter === "secondMajor") {
-      majorList = await majorDB.getMajorListByUniversityId(
-        client,
-        universityId,
-        [true, false],
-        [true],
-      );
+      isFirstMajor = [true, false];
+      isSecondMajor = [true];
+      invisibleMajorNames = [...NO_INFO, ...NO_MAJOR];
     } else {
       return res
         .status(statusCode.BAD_REQUEST)
         .send(util.fail(statusCode.BAD_REQUEST, responseMessage.INCORRECT_FILTER));
     }
 
+    const majorList = await majorDB.getMajorListByUniversityId(
+      client,
+      universityId,
+      isFirstMajor,
+      isSecondMajor,
+      invisibleMajorNames,
+    );
+
+    // separate major by default major, other major, other campus
+    let defaultMajor = [],
+      otherMajor = [],
+      otherCampus = [];
+
+    majorList.map((m) => {
+      if ([...NOT_ENTERED, ...NO_MAJOR].includes(m.majorName)) {
+        otherMajor.push(m);
+      } else if (m.majorName.includes("(")) {
+        otherCampus.push(m);
+      } else {
+        defaultMajor.push(m);
+      }
+    });
+
+    // sort by korean, alphabetical order
+    const isCharKorean = (char) => {
+      return char >= "가" && char <= "힣";
+    };
+
+    defaultMajor = _.sortBy(defaultMajor, [(m) => !isCharKorean(m.majorName[0]), "majorName"]);
+    otherCampus = _.sortBy(otherCampus, [(m) => !isCharKorean(m.majorName[0]), "majorName"]);
+
+    // other major first, followed by default major, other campus
+    const result = otherMajor.concat(defaultMajor, otherCampus);
+
     res
       .status(statusCode.OK)
-      .send(util.success(statusCode.OK, responseMessage.READ_ALL_MAJORS_SUCCESS, majorList));
+      .send(util.success(statusCode.OK, responseMessage.READ_ALL_MAJORS_SUCCESS, result));
   } catch (error) {
     errorHandlers.error(req, error);
 
