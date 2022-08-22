30b12196b54d0f5e5471d143f7367cabc47111af
juhyeon <wngus4296@swu.ac.kr>
2022-08-22 10:24:04 +0900

    [FIX] change route order

--- functions/api/routes/review/index.js
+++ functions/api/routes/review/index.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.post("/", checkUser, require("./reviewPOST"));
 router.delete("/:id", checkUser, require("./reviewDELETE"));
+router.get("/tag", checkUser, require("./reviewTagListGET"));
 router.get("/:id", checkUser, require("./reviewDetailGET"));
-router.get("/tag/list", checkUser, require("./reviewTagListGET"));
 router.get("/major/:majorId", checkUser, require("./reviewMajorListGET"));
 router.put("/:id", checkUser, require("./reviewPUT"));
 router.get("/university/:universityId", checkUser, require("./reviewUniversityListGET"));
