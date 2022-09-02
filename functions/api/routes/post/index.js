583b4c042e4112c51c3c8fc3110e18cca5f9f269
seoljiwon <suljiwon@naver.com>
2022-09-02 20:46:45 +0900

    [FIX] change major id to query

--- functions/api/routes/post/index.js
+++ functions/api/routes/post/index.js
@@ -6,6 +6,6 @@ router.post("/", checkUser, require("./postPOST"));
 router.delete("/:postId", checkUser, require("./postDELETE"));
 router.put("/:postId", checkUser, require("./postPUT"));
 router.get("/:postId", checkUser, require("./postGET"));
-router.get("/university/:universityId/major/:majorId", checkUser, require("./postListGET"));
+router.get("/university/:universityId", checkUser, require("./postListGET"));
 
 module.exports = router;
