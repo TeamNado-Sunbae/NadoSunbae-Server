9236f1e5e81de82cae138a888a63d83662e76bd0
juhyeon <wngus4296@swu.ac.kr>
2022-09-04 14:52:11 +0900

    [ADD] add app, auth swagger

--- functions/api/routes/app/index.js
+++ functions/api/routes/app/index.js
@@ -1,8 +1,46 @@
 const express = require("express");
 const router = express.Router();
 
+/**
+ * @swagger
+ * /app/link:
+ *   get:
+ *     summary: 앱 링크 조회
+ *     tags: [app]
+ *     responses:
+ *       200:
+ *         description: 앱 링크 조회 성공
+ *       500:
+ *         description: 서버 내부 에러
+ */
 router.get("/link", require("./appLinkGET"));
+
+/**
+ * @swagger
+ * /app/banner:
+ *   get:
+ *     summary: 앱 배너 리스트 조회
+ *     tags: [app]
+ *     responses:
+ *       200:
+ *         description: 앱 배너 리스트 조회 성공
+ *       500:
+ *         description: 서버 내부 에러
+ */
 router.get("/banner", require("./bannerListGET"));
+
+/**
+ * @swagger
+ * /app/version/recent:
+ *   get:
+ *     summary: 앱 최신 버전 조회
+ *     tags: [app]
+ *     responses:
+ *       200:
+ *         description: 앱 최신 버전 조회 성공
+ *       500:
+ *         description: 서버 내부 에러
+ */
 router.get("/version/recent", require("./versionRecentGET"));
 
 module.exports = router;
--- functions/api/routes/auth/index.js
+++ functions/api/routes/auth/index.js
@@ -2,7 +2,58 @@ const express = require("express");
 const { checkUser } = require("../../../middlewares/auth");
 const router = express.Router();
 
+/**
+ * @swagger
+ * /auth/duplication-check/nickname:
+ *   post:
+ *     summary: 닉네임 중복 체크
+ *     tags: [auth]
+ *     requestBody:
+ *       content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  nickname:
+ *                     type: string
+ *                     example: juhyeon
+ *     responses:
+ *       200:
+ *         description: 사용 가능한 닉네임
+ *       400:
+ *         description: 필요한 값 누락
+ *       409:
+ *         description: 이미 사용 중인 닉네임
+ *       500:
+ *         description: 서버 내부 에러
+ */
 router.post("/duplication-check/nickname", require("./authDuplicationCheckNicknamePOST"));
+
+/**
+ * @swagger
+ * /auth/duplication-check/email:
+ *   post:
+ *     summary: 이메일 중복 체크
+ *     tags: [auth]
+ *     requestBody:
+ *       content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                 email:
+ *                   type: string
+ *                   example: wngus4296@swu.ac.kr
+ *     responses:
+ *       200:
+ *         description: 사용 가능한 이메일
+ *       400:
+ *         description: 필요한 값 누락
+ *       409:
+ *         description: 이미 사용 중인 이메일
+ *       500:
+ *         description: 서버 내부 에러
+ */
 router.post("/duplication-check/email", require("./authDuplicationCheckEmailPOST"));
 router.post("/signup", require("./authSignupPOST"));
 router.post("/login", require("./authLoginPOST"));
