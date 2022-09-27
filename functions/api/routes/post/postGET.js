744fc08af474b36fb09ee1b56e0f79923a89f73d
juhyeon <wngus4296@swu.ac.kr>
2022-09-27 18:04:12 +0900

    [FIX] add answererId to post res

--- functions/api/routes/post/postGET.js
+++ functions/api/routes/post/postGET.js
@@ -94,6 +94,7 @@ module.exports = async (req, res) => {
           secondMajorName: post.secondMajorName,
           secondMajorStart: post.secondMajorStart,
         },
+        answererId: post.answererId ? post.answererId : null,
         // 1:1 하단 플로팅 버튼을 위한 권한 확인
         isAuthorized: req.user.id === post.writerId || req.user.id === post.answererId,
         like: {
