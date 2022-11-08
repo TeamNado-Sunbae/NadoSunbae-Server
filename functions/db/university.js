414e041948d307fae1df1c1a885b4d56f226217c
seoljiwon <76513385+seoljiwon@users.noreply.github.com>
2022-11-08 08:36:15 +0100

    feat: add university name to review slack message (#403)
    * feat: add university name to review slack message
    
    * [CHORE] add \n to slack message
    
    Co-authored-by: juhyeon <wngus4296@swu.ac.kr>

--- functions/db/university.js
+++ functions/db/university.js
@@ -24,7 +24,23 @@ const getNameByUniversityId = async (client, universityId) => {
   return convertSnakeToCamel.keysToCamel(rows[0]);
 };
 
+const getNameByMajorId = async (client, majorId) => {
+  const { rows } = await client.query(
+    `
+    SELECT university_name 
+    FROM university u
+    INNER JOIN major m
+    ON m.university_id = u.id
+    AND m.id = $1
+    AND is_deleted = false
+    `,
+    [majorId],
+  );
+  return convertSnakeToCamel.keysToCamel(rows[0]);
+};
+
 module.exports = {
   getEmailByUniversityId,
   getNameByUniversityId,
+  getNameByMajorId,
 };
